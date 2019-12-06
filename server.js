d143c05137065ff79d212df7f3d4793c27a02702
sross9845 <sean.ross.985@gmail.com>
2019-12-06 09:12:10 -0800

    first commit

--- server.js
+++ server.js
@@ -3,13 +3,13 @@ const express = require('express')
 const mongoose = require('mongoose')
 const expressJWT = require('express-jwt')
 const helmet = require('helmet')
-
+const cors = require('cors')
 const app = express()
 
 app.use(express.urlencoded({ extended: true}))
 app.use(express.json())
 app.use(helmet())
-
+app.use(cors())
 mongoose.connect(process.env.MONGODB_URL, {useNewUrlParser: true, useUnifiedTopology: true})
 const db = mongoose.connection
 db.once('open', () => {
@@ -25,6 +25,9 @@ app.use('/locked',
     expressJWT({secret: process.env.JWT_SECRET}).unless({method: 'POST'}),
     require('./routes/locked'))
 
+    // app.get('/*', (req, res) => {
+    //     res.sendFile(__dirname + '/client/public/index.html');
+    //   });
 app.listen(process.env.PORT, () => {
     console.log(`you are listening to the sweet sounds of port ${process.env.PORT}`)
 })
